f8f01b4205ed0c7daf102d86deec0cfe1eaffa50
Isrem_ <32309148+isaacdemeers@users.noreply.github.com>
2024-02-12 16:19:42 +0100

    Physics

---
+++ examples/vive/js/main.js
@@ -0,0 +1,14 @@
+
+var sceneEl = document.querySelector('#scene').innerHTML;
+
+var entityEl = '<a-sphere class="cube" dynamic-body position="0 2.25 1" radius="0.25" color="#EF2D5E"></a-sphere>'
+
+console.log(entityEl)
+
+sceneEl += entityEl
+
+console.log(sceneEl)
+
+
+
+
--- node_modules/markserv/Linked-Markdown-Example.md
+++
@@ -1,5 +0,0 @@
-#Linked Markdown Example
-
-Yay! :)
-
-![Skateboarding Dog](http://media.giphy.com/media/yN6TNQhiIxeW4/giphy.gif)
